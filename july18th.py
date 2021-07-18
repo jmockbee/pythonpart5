e97501ff21673c9781a802ae8007624b12bc3269
Jon Mockbee <mockbee1000@gmail.com>
2021-07-18 10:13:29 -0400

    need start end and how long

---
+++ july18th.py
@@ -0,0 +1,8 @@
+hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+print('end time: ')
+
+
+# Write your code here.
