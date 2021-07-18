f7e2e115eb2fda53e116db72a9b29237c8cabec3
Jon Mockbee <mockbee1000@gmail.com>
2021-07-18 10:35:23 -0400

    hour plus minutes divided by 60 diveded by 24

--- july18th.py
+++ july18th.py
@@ -2,7 +2,10 @@ hour = int(input("Starting time (hours): "))
 mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
 
-print('end time: ')
+# floor division returns the integer value of the the quotient. (it dumps the decimal)
+#  dura//60
+
+print('end time: ', ((hour + (min*dura)//60)%24)                                    %60  )
 
 
 # Write your code here.
